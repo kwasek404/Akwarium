c0d5e74840457e0a2222bf33c80163024bafb4c4
Kwasek <kwasek@kwasek.org>
2021-07-04 02:33:46 +0200

    #3 screen timera + zapis odczyt eeprom

--- app/sterownik_oswietlenia/main/main.ino
+++ app/sterownik_oswietlenia/main/main.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <virtuabotixRTC.h>
 #include <time.h>
+#include <EEPROM.h>
 
 const int buttonRightPin = 12;
 const int buttonSetPin = 11;
@@ -8,6 +9,12 @@ const int buttonMinusPin = 10;
 const int buttonPlusPin = 9;
 const int screenSelectSize = 3;
 const int activityBacklightSeconds = 60;
+const int eepromTimerStartHour = 0;
+const int eepromTimerStartMinute = 1;
+const int eepromTimerStartSecond = 2;
+const int eepromTimerStopHour = 3;
+const int eepromTimerStopMinute = 4;
+const int eepromTimerStopSecond = 5;
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 virtuabotixRTC myRTC(6, 7, 8); //If you change the wiring change the pins here also
@@ -18,6 +25,12 @@ int buttonMinusState = LOW;
 int buttonPlusState = LOW;
 time_t lastActivity = 0;
 bool backlightLast = false;
+int timerStartHour = 0;
+int timerStartMinute = 0;
+int timerStartSecond = 0;
+int timerStopHour = 0;
+int timerStopMinute = 0;
+int timerStopSecond = 0;
 
 void setup()
 {
@@ -25,6 +38,7 @@ void setup()
   lcd.init();
   setLastActivity();
   setScreenBacklight();
+  eepromLoad();
 }
 
 void loop() {
@@ -39,7 +53,7 @@ void loop() {
       screenDate();
       break;
     case 2:
-      screenBrightness();
+      screenTimer();
       break;
   }
   delay(100);
@@ -58,9 +72,110 @@ void screenInfo() {
   lcd.print("Power: 200W 100%");
 }
 
-void screenBrightness() {
+void eepromLoad() {
+  timerStartHour = EEPROM.read(eepromTimerStartHour);
+  if (timerStartHour >= 24) timerStartHour = 0;
+  timerStartMinute = EEPROM.read(eepromTimerStartMinute);
+  if (timerStartMinute >= 60) timerStartMinute = 0;
+  timerStartSecond = EEPROM.read(eepromTimerStartSecond);
+  if (timerStartSecond >= 60) timerStartSecond = 0;
+  timerStopHour = EEPROM.read(eepromTimerStopHour);
+  if (timerStopHour >= 24) timerStopHour = 0;
+  timerStopMinute = EEPROM.read(eepromTimerStopMinute);
+  if (timerStopMinute >= 60) timerStopMinute = 0;
+  timerStopSecond = EEPROM.read(eepromTimerStopSecond);
+  if (timerStopSecond >= 60) timerStopSecond = 0;
+}
+
+void eepromSave() {
+  EEPROM.write(eepromTimerStartHour, timerStartHour);
+  EEPROM.write(eepromTimerStartMinute, timerStartMinute);
+  EEPROM.write(eepromTimerStartSecond, timerStartSecond);
+  EEPROM.write(eepromTimerStopHour, timerStopHour);
+  EEPROM.write(eepromTimerStopMinute, timerStopMinute);
+  EEPROM.write(eepromTimerStopSecond, timerStopSecond);
+}
+
+void screenTimer() {
+  char line[2][16];
+  sprintf(line[0], "Start: %.2d:%.2d:%.2d ", timerStartHour, timerStartMinute, timerStartSecond);
+  sprintf(line[1], "Stop:  %.2d:%.2d:%.2d ", timerStopHour, timerStopMinute, timerStopSecond);
   lcd.setCursor(0, 0);
-  lcd.print("Brightness");
+  lcd.print(line[0]);
+  lcd.setCursor(0, 1);
+  lcd.print(line[1]);
+  if (buttonSetState == HIGH) setScreenTimer();
+}
+
+void setScreenTimer() {
+  char timeStart[9];
+  char timeStop[9];
+  int selectPos = 0;
+  int menuSize = 6;
+  char line[2][16];
+  bool display = true;
+
+  do {
+    setButton();
+    sprintf(timeStart, "%.2d:%.2d:%.2d", timerStartHour, timerStartMinute, timerStartSecond);
+    sprintf(timeStop, "%.2d:%.2d:%.2d", timerStopHour, timerStopMinute, timerStopSecond);
+
+    if (buttonRightState == HIGH) {
+      selectPos += 1;
+      if (selectPos >= menuSize) {
+        selectPos = 0;
+      }
+    }
+
+    switch (selectPos) {
+      case 0:
+        if (!display) sprintf(timeStart, "  :%.2d:%.2d", timerStartMinute, timerStartSecond);
+        if (buttonPlusState == HIGH) timerStartHour += 1;
+        if (buttonMinusState == HIGH) timerStartHour += -1;
+        break;
+      case 1:
+        if (!display) sprintf(timeStart, "%.2d:  :%.2d", timerStartHour, timerStartSecond);
+        if (buttonPlusState == HIGH) timerStartMinute += 1;
+        if (buttonMinusState == HIGH) timerStartMinute += -1;
+        break;
+      case 2:
+        if (!display) sprintf(timeStart, "%.2d:%.2d:  ", timerStartHour, timerStartMinute);
+        if (buttonPlusState == HIGH) timerStartSecond += 1;
+        if (buttonMinusState == HIGH) timerStartSecond += -1;
+        break;
+      case 3:
+        if (!display) sprintf(timeStop, "  :%.2d:%.2d", timerStopMinute, timerStopSecond);
+        if (buttonPlusState == HIGH) timerStopHour += 1;
+        if (buttonMinusState == HIGH) timerStopHour += -1;
+        break;
+      case 4:
+        if (!display) sprintf(timeStop, "%.2d:  :%.2d", timerStopHour, timerStopSecond);
+        if (buttonPlusState == HIGH) timerStopMinute += 1;
+        if (buttonMinusState == HIGH) timerStopMinute += -1;
+        break;
+      case 5:
+        if (!display) sprintf(timeStop, "%.2d:%.2d:  ", timerStopHour, timerStopMinute);
+        if (buttonPlusState == HIGH) timerStopSecond += 1;
+        if (buttonMinusState == HIGH) timerStopSecond += -1;
+        break;
+    }
+
+    if (display) {
+      display = false;
+    } else {
+      display = true;
+    }
+
+    sprintf(line[0], "Start: %s ", timeStart);
+    sprintf(line[1], "Stop:  %s ", timeStop);
+    lcd.setCursor(0, 0);
+    lcd.print(line[0]);
+    lcd.setCursor(0, 1);
+    lcd.print(line[1]);
+
+    delay(100);
+  } while (buttonSetState == LOW);
+  eepromSave();
 }
 
 void screenDate() {
@@ -79,9 +194,7 @@ void screenDate() {
   sprintf(line[1], "Date: %s", date);
   lcd.print(line[1]);
 
-  if (buttonSetState == HIGH) {
-    setScreenDate();
-  }
+  if (buttonSetState == HIGH) setScreenDate();
 }
 
 void adjustTimeDate(struct tm * timeinfo, int hour, int minute, int second, int year, int month, int day) {
