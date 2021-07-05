79e1ab198dcb371019516710eab3f49b7f496680
Kwasek <kwasek@kwasek.org>
2021-07-05 21:10:11 +0200

    #3 kalkulacja mocy oświetlenia

--- app/sterownik_oswietlenia/main/main.ino
+++ app/sterownik_oswietlenia/main/main.ino
@@ -2,6 +2,7 @@
 #include <virtuabotixRTC.h>
 #include <time.h>
 #include <EEPROM.h>
+#include <math.h>
 
 const int buttonRightPin = 12;
 const int buttonSetPin = 11;
@@ -15,6 +16,8 @@ const int eepromTimerStartSecond = 2;
 const int eepromTimerStopHour = 3;
 const int eepromTimerStopMinute = 4;
 const int eepromTimerStopSecond = 5;
+const int xPow = 2;
+const float yPowMax = 13.4; //6.7 = 103%
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 virtuabotixRTC myRTC(6, 7, 8); //If you change the wiring change the pins here also
@@ -31,10 +34,11 @@ int timerStartSecond = 0;
 int timerStopHour = 0;
 int timerStopMinute = 0;
 int timerStopSecond = 0;
+float powerPercent = 0;
 
 void setup()
 {
-  // Serial.begin(9600);
+  Serial.begin(9600);
   lcd.init();
   setLastActivity();
   setScreenBacklight();
@@ -45,6 +49,7 @@ void loop() {
   setButton();
   screenSelect();
   setScreenBacklight();
+  calculatePower();
   switch (screenSelectCurrent) {
     case 0:
       screenInfo();
@@ -60,16 +65,59 @@ void loop() {
 }
 
 void screenInfo() {
-  struct tm * timeinfo = getCurrentTm();
+  struct tm timeinfo = getCurrentTm();
   char time[9];
   char date[11];
   char line[2][16];
-  strftime(time, 9, "%H:%M:%S", timeinfo);
+  strftime(time, 9, "%H:%M:%S", &timeinfo);
   lcd.setCursor(0, 0);
   sprintf(line[0], "Time: %s ", time);
   lcd.print(line[0]);
   lcd.setCursor(0, 1);
-  lcd.print("Power: 200W 100%");
+  sprintf(line[1], "Power: %s%%    ", String(powerPercent).c_str());
+  lcd.print(line[1]);
+}
+
+void calculatePower() {
+  struct tm tmStart = getCurrentTm();
+  tmStart.tm_hour = timerStartHour;
+  tmStart.tm_min = timerStartMinute;
+  tmStart.tm_sec = timerStartSecond;
+  struct tm tmStop = getCurrentTm();
+  tmStop.tm_hour = timerStopHour;
+  tmStop.tm_min = timerStopMinute;
+  tmStop.tm_sec = timerStopSecond;
+
+  time_t timestampNow = getCurrentTimestamp();
+  time_t timestampStart = mktime(&tmStart);
+  time_t timestampStop = mktime(&tmStop);
+
+  if (timestampStart >= timestampStop) {
+    tmStop.tm_mday += 1;
+    timestampStop = mktime(&tmStop);
+  }
+
+  if (! ((timestampStart <= timestampNow) && (timestampStop >= timestampNow))) {
+    powerPercent = 0;
+    return ;
+  }
+
+  time_t timestampstopstartdiff = timestampStop - timestampStart;
+  float yPow;
+
+  if ((timestampStart + (timestampstopstartdiff / 2)) >= timestampNow) {
+    //rozjasnianie
+    Serial.println("rozjasnianie");
+    yPow = ((yPowMax * (timestampNow - timestampStart)) / (timestampstopstartdiff / 2));
+  } else {
+    //przyciemnianie
+    Serial.println("przyciemnianie");
+    yPow = ((yPowMax * (timestampStop - timestampNow)) / (timestampstopstartdiff / 2));
+    
+  }
+
+  powerPercent = pow(xPow, yPow);
+  if (powerPercent > 100.0) powerPercent = 100.0;
 }
 
 void eepromLoad() {
@@ -179,13 +227,13 @@ void setScreenTimer() {
 }
 
 void screenDate() {
-  struct tm * timeinfo = getCurrentTm();
+  struct tm timeinfo = getCurrentTm();
   char time[9];
   char date[11];
   char line[2][16];
 
-  strftime(time, 9, "%H:%M:%S", timeinfo);
-  strftime(date, 11, "%Y/%m/%d", timeinfo);
+  strftime(time, 9, "%H:%M:%S", &timeinfo);
+  strftime(date, 11, "%Y/%m/%d", &timeinfo);
 
   lcd.setCursor(0, 0);
   sprintf(line[0], "Time: %s ", time);
@@ -210,12 +258,12 @@ void setScreenDate() {
   bool display = true;
   int selectPos = 0;
   int menuSize = 6;
-  struct tm * timeinfo;
+  struct tm timeinfo;
   do {
     setButton();
     timeinfo = getCurrentTm();
-    strftime(time, 9, timeFormat, timeinfo);
-    strftime(date, 11, dateFormat, timeinfo);
+    strftime(time, 9, timeFormat, &timeinfo);
+    strftime(date, 11, dateFormat, &timeinfo);
 
     if (buttonRightState == HIGH) {
       selectPos += 1;
@@ -226,34 +274,34 @@ void setScreenDate() {
 
     switch (selectPos) {
       case 0:
-        if (!display) strftime(time, 9, "  :%M:%S", timeinfo);
-        if (buttonPlusState == HIGH) adjustTimeDate(timeinfo, 1, 0, 0, 0, 0, 0);
-        if (buttonMinusState == HIGH) adjustTimeDate(timeinfo, -1, 0, 0, 0, 0, 0);
+        if (!display) strftime(time, 9, "  :%M:%S", &timeinfo);
+        if (buttonPlusState == HIGH) adjustTimeDate(&timeinfo, 1, 0, 0, 0, 0, 0);
+        if (buttonMinusState == HIGH) adjustTimeDate(&timeinfo, -1, 0, 0, 0, 0, 0);
         break;
       case 1:
-        if (!display) strftime(time, 9, "%H:  :%S", timeinfo);
-        if (buttonPlusState == HIGH) adjustTimeDate(timeinfo, 0, 1, 0, 0, 0, 0);
-        if (buttonMinusState == HIGH) adjustTimeDate(timeinfo, 0, -1, 0, 0, 0, 0);
+        if (!display) strftime(time, 9, "%H:  :%S", &timeinfo);
+        if (buttonPlusState == HIGH) adjustTimeDate(&timeinfo, 0, 1, 0, 0, 0, 0);
+        if (buttonMinusState == HIGH) adjustTimeDate(&timeinfo, 0, -1, 0, 0, 0, 0);
         break;
       case 2:
-        if (!display) strftime(time, 9, "%H:%M:  ", timeinfo);
-        if (buttonPlusState == HIGH) adjustTimeDate(timeinfo, 0, 0, 1, 0, 0, 0);
-        if (buttonMinusState == HIGH) adjustTimeDate(timeinfo, 0, 0, -1, 0, 0, 0);
+        if (!display) strftime(time, 9, "%H:%M:  ", &timeinfo);
+        if (buttonPlusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, 1, 0, 0, 0);
+        if (buttonMinusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, -1, 0, 0, 0);
         break;
       case 3:
-        if (!display) strftime(date, 11, "    /%m/%d", timeinfo);
-        if (buttonPlusState == HIGH) adjustTimeDate(timeinfo, 0, 0, 0, 1, 0, 0);
-        if (buttonMinusState == HIGH) adjustTimeDate(timeinfo, 0, 0, 0, -1, 0, 0);
+        if (!display) strftime(date, 11, "    /%m/%d", &timeinfo);
+        if (buttonPlusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, 0, 1, 0, 0);
+        if (buttonMinusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, 0, -1, 0, 0);
         break;
       case 4:
-        if (!display) strftime(date, 11, "%Y/  /%d", timeinfo);
-        if (buttonPlusState == HIGH) adjustTimeDate(timeinfo, 0, 0, 0, 0, 1, 0);
-        if (buttonMinusState == HIGH) adjustTimeDate(timeinfo, 0, 0, 0, 0, -1, 0);
+        if (!display) strftime(date, 11, "%Y/  /%d", &timeinfo);
+        if (buttonPlusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, 0, 0, 1, 0);
+        if (buttonMinusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, 0, 0, -1, 0);
         break;
       case 5:
-        if (!display) strftime(date, 11, "%Y/%m/  ", timeinfo);
-        if (buttonPlusState == HIGH) adjustTimeDate(timeinfo, 0, 0, 0, 0, 0, 1);
-        if (buttonMinusState == HIGH) adjustTimeDate(timeinfo, 0, 0, 0, 0, 0, -1);
+        if (!display) strftime(date, 11, "%Y/%m/  ", &timeinfo);
+        if (buttonPlusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, 0, 0, 0, 1);
+        if (buttonMinusState == HIGH) adjustTimeDate(&timeinfo, 0, 0, 0, 0, 0, -1);
         break;
     }
 
@@ -306,23 +354,22 @@ bool getDigitalButton(int pin) {
   return state;
 }
 
-struct tm * getCurrentTm() {
-  time_t tmpNow = 0;
-  struct tm * timeinfo = localtime(&tmpNow);
+struct tm getCurrentTm() {
+  struct tm timeinfo;
   myRTC.updateTime();
-  timeinfo->tm_year = myRTC.year - 1900;
-  timeinfo->tm_mon = myRTC.month - 1;
-  timeinfo->tm_mday = myRTC.dayofmonth;
-  timeinfo->tm_hour = myRTC.hours;
-  timeinfo->tm_min = myRTC.minutes;
-  timeinfo->tm_sec = myRTC.seconds;
-  timeinfo->tm_isdst = -1;
+  timeinfo.tm_year = myRTC.year - 1900;
+  timeinfo.tm_mon = myRTC.month - 1;
+  timeinfo.tm_mday = myRTC.dayofmonth;
+  timeinfo.tm_hour = myRTC.hours;
+  timeinfo.tm_min = myRTC.minutes;
+  timeinfo.tm_sec = myRTC.seconds;
+  timeinfo.tm_isdst = -1;
   return timeinfo;
 }
 
 time_t getCurrentTimestamp() {
-  struct tm * timeinfo = getCurrentTm();
-  return mktime(timeinfo);
+  struct tm timeinfo = getCurrentTm();
+  return mktime(&timeinfo);
 }
 
 void setLastActivity() {
